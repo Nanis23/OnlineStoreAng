4f36143661a83d267235cb8e5bb15ed21d2ff43b
Sinan <sinajsinan1@gmail.com>
2024-11-02 12:47:53 +0100

    xyz

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,9 +6,7 @@ import { HeaderComponent } from './auth/dashboard/component-dashboard/header/hea
 import { FooterComponent } from './auth/dashboard/component-dashboard/footer/footer.component';
 import { SidebarComponent } from './auth/dashboard/component-dashboard/sidebar/sidebar.component';
 import { DashboardLayoutComponent } from './auth/dashboard/dashboard-layout/dashboard-layout.component';
-import { RegisterUserComponent } from './auth/dashboard/user/register-user/register-user.component';
-import { UpdateUserComponent } from './auth/dashboard/user/update-user/update-user.component';
-import { DeleteUserComponent } from './auth/dashboard/user/delete-user/delete-user.component';
+
 import { CategoryComponent } from './components/category/category.component';
 import { UserComponent } from './components/user/user.component';
 import { ProductComponent } from './components/product/product.component'; 
@@ -24,9 +22,7 @@ export const routes: Routes = [
     { path: "footer", component: FooterComponent },
     { path: "sidebar", component: SidebarComponent },
     { path: "dashboard", component: DashboardLayoutComponent },
-    { path: "registerUser", component: RegisterUserComponent },
-    { path: "updateUser", component: UpdateUserComponent },
-    { path: "deleteUSer", component: DeleteUserComponent },
+
     { path: "listCategory",component:CategoryComponent},
     { path: "user",component:UserComponent},
     { path: "product",component:ProductComponent},
